28085293c2d19119750c66aca63db3b72fb0d755
Mikael Gramont <mgramont@gmail.com>
2020-05-03 16:31:43 +0200

    Added SASS, added onChange functions

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 
 import Slider from '../components/Slider';
-import styles from '../styles/home.module.css';
+import styles from '../styles/home.module.scss';
 
 const title = 'React Kicker';
 
 export default function Home() {
+  const onHeightChange = () => {};
+  const onWidthChange = () => {};
+  const onRadiusChange = () => {};
+
   return (
     <div className="container">
       <Head>
@@ -16,9 +20,9 @@ export default function Home() {
         <h1 className="title">{title}</h1>
 
         <div className={styles.input}>
-          <Slider className={styles.slider} label="height" id="height" name="height" min={0.5} max={3} value={1.0} />
-          <Slider className={styles.slider} label="width" id="width" name="width" min={0.5} max={3} value={1.2} />
-          <Slider className={styles.slider} label="radius" id="radius" name="radius" min={30} max={89} value={45}/>
+          <Slider onChange={onHeightChange} className={styles.slider} label="height" id="height" name="height" min={0.5} max={3} value={1.0} />
+          <Slider onChange={onWidthChange} className={styles.slider} label="width" id="width" name="width" min={0.5} max={3} value={1.2} />
+          <Slider onChange={onRadiusChange} className={styles.slider} label="radius" id="radius" name="radius" min={30} max={89} value={45}/>
         </div>
 
         <div className={styles.output}>
