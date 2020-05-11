06837d5dcc5a00e99ceb264765171d305cd49d27
Mikael Gramont <mgramont@gmail.com>
2020-05-11 10:08:48 +0200

    Updated README and left comment in config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(['three', 'drei']);
 
+// https://github.com/react-spring/drei/issues/2
 module.exports = withPlugins([withTM], {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
